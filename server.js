376bd50bbd047ea5021740d589a66a068e3bb1e1
Gagan-Nagaraju <gagann.n7@gmail.com>
2015-12-13 05:09:34 -0500

    Node API routes

--- server.js
+++ server.js
@@ -1,28 +1,78 @@
 // server.js
 
-    // set up ========================
+    /*               set up                */
     var express  = require('express');
-    var app      = express();                               // create our app w/ express
-    var mongoose = require('mongoose');                     // mongoose for mongodb
-    var morgan = require('morgan');             // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+    var app      = express();                               // create the app
+    var mongoose = require('mongoose');                     // Mongo DB
+    var morgan = require('morgan');                         // log requests to the console
+    var bodyParser = require('body-parser');                // parse the req body
+    var methodOverride = require('method-override');        // simulate DELETE and PUT 
 
-    // configuration =================
+    /*               configuration          */
 
-    mongoose.connect('mongodb://<user>:<pass>@apollo.modulusmongo.net:27017/zyn4oTuj');     // connect to mongoDB database on modulus.io
+    mongoose.connect('mongodb://<user>:<pass>@apollo.modulusmongo.net:27017/zyn4oTuj'); // connect to mongoDB database
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(express.static(__dirname + '/public'));                 
+    app.use(morgan('dev'));                                         
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-    // define model =================
+    /*               Define model           */
     var Todo = mongoose.model('Todo', {
     text : String
     });
 
+    /*                   Creating API routes                 */
 
+    // GET
+    app.get('/api/todos', function(req, res) {
 
+        Todo.find(function(err, todos) {
+
+            
+            if (err)
+                res.send(err)
+
+            res.json(todos); 
+        });
+    });
+
+    // POST
+    app.post('/api/todos', function(req, res) {
+
+        
+        Todo.create({
+            text : req.body.text,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // return all the todos in the DB
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+
+    });
+
+    // DELETE
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+           // return all todos after the operation
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
